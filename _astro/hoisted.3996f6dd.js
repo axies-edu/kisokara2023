deb2c873575e16f39e5dd5b363d5dcea37ab58a3
ixkaito <ixkaito@users.noreply.github.com>
2025-03-27 07:43:27 +0000

    Build static files

---
+++ _astro/hoisted.3996f6dd.js
@@ -0,0 +1,1 @@
+import{p as r}from"./popover.b063bd20.js";r(".js-nav-trigger",".js-nav-container");
---
+++ _astro/setDefaultVolume.87b88bef.js
@@ -0,0 +1,1 @@
+const t=e=>{document.querySelectorAll("video").forEach(o=>{o.volume=e})};export{t as s};
